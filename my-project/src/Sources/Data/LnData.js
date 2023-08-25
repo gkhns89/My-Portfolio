827f15eeba7e24e848fce1ec191b106093e0fa4e
gkhns89 <gokhansisman@outlook.com>
2023-08-25 04:15:07 +0300

    translate

--- my-project/src/Sources/Data/LnData.js
+++ my-project/src/Sources/Data/LnData.js
@@ -20,19 +20,19 @@ export const contentData = {
           id: 0,
           title: "Java Script",
           descr:
-            "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!",
+            "JavaScript is a programming language that, along with HTML and CSS, is one of the core technologies of the World Wide Web. More than 97% of websites use client-side JavaScript for web page gestures, and the code used often includes third-party libraries.",
         },
         {
           id: 1,
           title: "React.JS",
           descr:
-            "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!",
+            "React is an open source javascript library for creating user interfaces. React, which is being developed by a developer group led by Facebook, was created in accordance with the Model-View-Controller principle.",
         },
         {
           id: 2,
           title: "Node.JS",
           descr:
-            "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!",
+            "Node.js is an open source runtime environment for networked applications, typically server-side. Node.js applications are typically developed using JavaScript, a client-side scripting language.",
         },
       ],
     },
@@ -98,12 +98,12 @@ export const contentData = {
       to: " TO SWITCH",
       skills: "Yetenekler",
       projects: "Projeler",
-      hire: "Bana Para VER!",
+      hire: "E-Posta",
     },
     hero: {
       mTitle: "Yaratıcı Düşünce Minimalist Düzen",
       info: "Merhaba, ben Gökhan. Full-Stack yazılımcı olmak için çalışıyorum. Eğer öğrenmeye hevesli, yeni meydan okumalara açık, yaratıcı birisini arıyorsanız, benimle iletişime geçebilirsiniz!",
-      hire: "BANA PARA VER!",
+      hire: "E-Posta",
     },
     skills: {
       mTitle: "Yetenekler",
@@ -112,19 +112,19 @@ export const contentData = {
           id: 0,
           title: "Java Script",
           descr:
-            "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!",
+            "JavaScript, HTML ve CSS ile birlikte World Wide Web'in temel teknolojilerinden biri olan bir programlama dilidir. Web sitelerinin %97'sinden fazlası, web sayfası hareketleri için istemci tarafı JavaScript kullanır ve kullanılan kod genellikle üçüncü taraf kitaplıkları içerir.",
         },
         {
           id: 1,
           title: "React.JS",
           descr:
-            "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!",
+            "React kullanıcı arayüzü oluşturmaya yarayan açık kaynak kodlu bir javascript kütüphanesidir. Facebook önderliğinde bir geliştirici grubu tarafından geliştirilmekte olan React, Model-View-Controller prensibine uygun olarak oluşturulmuştur.",
         },
         {
           id: 2,
           title: "Node.JS",
           descr:
-            "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!",
+            "Node.js, açık kaynaklı, genelde sunucu tarafında çalışan ve ağ bağlantılı uygulamalar için geliştirilmiş bir çalıştırma ortamıdır. Node.js uygulamaları genelde istemci tarafı betik dili olan JavaScript kullanılarak geliştirilir.",
         },
       ],
     },
